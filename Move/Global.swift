b2d7c0d146c4f7ec568ae98feab59709f0597f52
Edward Muya <muya@redpulse.co.ke>
2017-07-29 14:17:40 +0300

    UI Creation - tabbed view

---
+++ Move/Global.swift
@@ -0,0 +1,15 @@
+//
+//  Global.swift
+//  Move
+//
+//  Created by einmuya on 7/29/17.
+//  Copyright © 2017 Crafted. All rights reserved.
+//
+
+import UIKit
+
+struct K {
+    struct Base {
+        static let URL = "https://test.movetocure.appreciate.be/api/v1/wiki"
+    }
+}
